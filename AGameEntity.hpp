7650dbad530f2b4b72c0e9a2bc2fda65ea93d03c
Aymeric FAUCHER <afaucher@e3r10p10.42.fr>
2015-01-10 20:47:29 +0100

    AGameEntity

--- AGameEntity.hpp
+++ AGameEntity.hpp
@@ -1,18 +1,23 @@
 #ifndef AGAMEENTITY_HPP
 # define AGAMEENTITY_HPP
 
+#include "Position.hpp"
+#include "Hitbox.hpp"
+#include "Sprite.hpp"
+#include <iostream>
+#include <ssstream>
 class AGameEntity
 {
 	public:
-		AGameEntity(int x, int y, int wdt, int hei, Sprite const & sprite);
+		AGameEntity(int x, int y, int wdth, int hght, int speed, int maxspeed, Sprite const & sprite);
 		AGameEntity(AGameEntity const & src);
 		virtual ~AGameEntity( void );
 
 		AGameEntity &			operator=(AGameEntity const & rhs);
 
-		Position &				getPosition(void) const;
-		Hitbox &				getHitbox(void) const;
-		Sprite &				getSprite(void) const;
+		Position const &		getPosition(void) const;
+		Hitbox const &			getHitbox(void) const;
+		Sprite const &			getSprite(void) const;
 		int						getSpeed() const;
 
 		virtual void			refresh() = 0;
@@ -27,8 +32,10 @@ class AGameEntity
 		int						_dead;
 		int						_speed;
 		int						_maxspeed;
+		int						_index;
 
 	private:
+		static unsigned int		_nb_inst;
 		AGameEntity( void );
 };
 
