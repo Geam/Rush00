2c3916d33beb8d472d52aff48e8bc7b12fd10129
Aymeric FAUCHER <afaucher@e3r10p10.42.fr>
2015-01-10 22:32:39 +0100

    Pattern

--- Pattern.hpp
+++ Pattern.hpp
@@ -1,21 +1,28 @@
 #ifndef PATTERN_HPP
 # define PATTERN_HPP
 
-enum	e_direction = { NONE = 1, UP, DOWN, LEFT, RIGHT }
+#include <iostream>
+#include <string>
+#include <sstream>
+enum	e_direction { NONE = 0, UP = 1, DOWN = 2, LEFT = 3, RIGHT = 4};
 class Pattern
 {
 	public:
-		Pattern(std::string pattern);
+		Pattern(std::string const & pattern);
 		Pattern(Pattern const & src);
 		~Pattern( void );
 		Pattern &	operator=(Pattern const & rhs);
+		std::string	toString() const;
 
 		e_direction	next();
 
 	private:
 		std::string	_pattern;
 		int			_current;
+		int			_last;
 		Pattern( void );
 };
+
+std::ostream &			operator<<(std::ostream & o, Pattern const & rhs);
 
 #endif /* PATTERN_HPP */
