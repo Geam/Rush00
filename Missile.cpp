367a9bf3f5a6df3b99fe92fba239513b7dcf8005
mdelage <mdelage@student.42.fr>
2015-01-10 22:44:12 +0100

    4th draft

--- Missile.cpp
+++ Missile.cpp
@@ -1,13 +1,25 @@
 
 #include "Missile.hpp"
 
-Missile::Missile(int x, int y, std::string pattern, ACharacter const & owner) : _x(x), _y(y), _pattern(Pattern(pattern)), _owner(owner)
+Missile::Missile(Position & pos, Hitbox hb, Sprite sp, Pattern pattern, ACharacter const & owner, int speed) : _pattern(pattern), _owner(owner)
 {
+	this->_pos = pos;
+	this->_hb = hb;
+	this->_sp = sp;
+	this->_dead = false;
+	this->_speed = speed;
+	this->_maxspeed = speed;
 	return ;
 }
 
-Missile::Missile(Missile const & src) : _x(src.getX()), _y(src.getY()), _pattern(src.getPattern()), _owner(src.getOwner())
+Missile::Missile(Missile const & src) : _pattern(src.getPattern()), _owner(src.getOwner())
 {
+	this->_pos = src._pos;
+	this->_hb = src._hb;
+	this->_sp = src._sp;
+	this->_dead = src._dead;
+	this->_speed = src._speed;
+	this->_maxspeed = src._maxspeed;
 	return ;
 }
 
@@ -15,34 +27,31 @@ Missile::~Missile( void )
 {
 }
 
-Missile &	Missile::operator=(Missile const & rhs)
+Missile &			Missile::operator=(Missile const & rhs)
+{
+	this->_pos = rhs._pos;
+	this->_hb = rhs._hb;
+	this->_sp = rhs._sp;
+	this->_dead = rhs._dead;
+	this->_speed = rhs._speed;
+	this->_maxspeed = rhs._maxspeed;
+	return (*this);
+}
+
+void				Missile::refresh(void)
 {
 }
 
-void		Missile::refresh(void)
+void				Missile::destroy(void)
 {
 }
 
-void		Missile::destroy(void)
-{
-}
-
-int			Missile::getX( void ) const
-{
-	return (this->_x);
-}
-
-int			Missile::getY( void ) const
-{
-	return (this->_y);
-}
-
-Pattern &	Missile::getPattern( void ) const
+Pattern &			Missile::getPattern( void ) const
 {
 	return (this->_pattern);
 }
 
-ACharacter &	Missile::getOwner( void ) const
+ACharacter const &	Missile::getOwner( void ) const
 {
 	return (this->_owner);
 }
